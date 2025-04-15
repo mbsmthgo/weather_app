c794aea5eb0f86d1ac2b95bcabecc3d4392b7e5d
mbsmthgo <gordienko.lada@mail.ru>
2025-04-15 12:28:32 +0300

    Выведены значения погоды, добавлена интеграция с API погоды

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from 'react'
+import WEATHER_API_KEY from "./utils/constant.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
+    const [info, setInfo] = useState({})
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    function handleNameChange(event) {
+        setName(event.target.value)
+        console.log(name)
+    }
+
+    function handleButtonClick() {
+        fetchWeather(name)
+    }
+
+    function fetchWeather(address) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + address + "&units=metric&appid="+ WEATHER_API_KEY)
+            .then(function (response) {
+                return response.json()
+            }).then(function (data) {
+            console.log(data)
+            setInfo(data)
+        })
+
+    }
+
+    return (
+        <div className="background">
+            <div className="container">
+                <h1>Weather Forecast</h1>
+                <div className="container-searchbar">
+                    <label>
+                        <input
+                            placeholder="Enter the city name"
+                            value={name}
+                            onChange={handleNameChange}/>
+                    </label>
+                    <button onClick={handleButtonClick}>Search</button>
+                </div>
+            </div>
+            <div className="info">
+                <img className="sun" src="src/sun.png" alt="Sun"/>
+                <h1 className="city">{info.name}</h1>
+                <h2 className="temp">{info.main?.temp}°C</h2>
+                <div className="extra">
+                    <p className="humidity">{info.main?.humidity}%</p>
+                    <p className="wind">{info.wind?.speed} m/s</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
---
+++ src/utils/constant.js
@@ -0,0 +1,2 @@
+const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+export default WEATHER_API_KEY
